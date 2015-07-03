4a8ad8bc427c916cd27ac3507af19ee72aef0796
Thiago Caiubi <thiago.caiubi@gmail.com>
2015-07-03 20:15:26 -0300

    Add hash consistency algorithm under Hoop.Get method

--- hoop.go
+++ hoop.go
@@ -2,16 +2,36 @@ package hoop
 
 import (
 	"errors"
+	"fmt"
+	"hash/crc32"
+	"sort"
 )
 
 var ErrEmptyHoop = errors.New("hoop is empty")
 
 type Hoop struct {
-	nodes []string
+	replicas    int
+	nodesHashes []int
+	nodesMap    map[int]string
 }
 
-func New(nodes []string) *Hoop {
-	return &Hoop{nodes}
+func New(nodes []string, replicas int) *Hoop {
+	h := &Hoop{
+		replicas: replicas,
+		nodesMap: make(map[int]string),
+	}
+
+	for _, node := range nodes {
+		for i := 0; i < replicas; i++ {
+			hash := hash(fmt.Sprintf("%v:%v", i, node))
+			h.nodesHashes = append(h.nodesHashes, hash)
+			h.nodesMap[hash] = node
+		}
+	}
+
+	sort.Ints(h.nodesHashes)
+
+	return h
 }
 
 func (h *Hoop) Get(key string) (string, error) {
@@ -19,9 +39,23 @@ func (h *Hoop) Get(key string) (string, error) {
 		return "", ErrEmptyHoop
 	}
 
-	return "", nil
+	hash := hash(key)
+
+	pos := sort.Search(len(h.nodesHashes), func(i int) bool {
+		return h.nodesHashes[i] >= hash
+	})
+
+	if pos >= len(h.nodesHashes) {
+		pos = 0
+	}
+
+	return h.nodesMap[h.nodesHashes[pos]], nil
 }
 
 func (h *Hoop) isEmpty() bool {
-	return len(h.nodes) == 0
+	return len(h.nodesHashes) == 0
+}
+
+func hash(data string) int {
+	return int(crc32.ChecksumIEEE([]byte(data)))
 }
--- hoop_test.go
+++ hoop_test.go
@@ -4,12 +4,42 @@ import (
 	"testing"
 )
 
+const replicas = 3
+
 func TestEmpty(t *testing.T) {
-	h := New([]string{})
+	h := New([]string{}, replicas)
 
 	_, err := h.Get("key")
 
 	if err != ErrEmptyHoop {
 		t.Errorf("Expected \"%s\" but got \"%s\"", ErrEmptyHoop, err)
+	}
+}
+
+func TestGet(t *testing.T) {
+	nodes := []string{
+		"node0",
+		"node1",
+		"node2",
+	}
+
+	h := New(nodes, replicas)
+
+	node0, _ := h.Get("beta")
+
+	if node0 != nodes[0] {
+		t.Errorf("Expected \"%s\" but got \"%s\"", nodes[0], node0)
+	}
+
+	node1, _ := h.Get("alpha")
+
+	if node1 != nodes[1] {
+		t.Errorf("Expected \"%s\" but got \"%s\"", nodes[1], node1)
+	}
+
+	node2, _ := h.Get("lambda")
+
+	if node2 != nodes[2] {
+		t.Errorf("Expected \"%s\" but got \"%s\"", nodes[2], node2)
 	}
 }
